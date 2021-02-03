780423856991dcf32347eff9c56170a93507fc73
joy <ziaurjoy802@gmail.com>
2021-02-03 15:57:36 +0600

    searching algorithm done

---
+++ searching algorithm/linear_search.py
@@ -0,0 +1,81 @@
+
+# foor loop
+
+# def linear_search(l, x):
+#     for i in l:
+#         if l[i] == x:
+#             return i
+#     return -1
+# my_list = [1, 2, 3, 4, 5]
+# print(linear_search(my_list, 3))
+
+
+# while loop
+
+# def linear_search(l, x):
+#     i = 0
+#     while i < len(l):
+#         if l[i] == x:
+#             return i
+#         i += 1
+#     return -1
+#
+# my_list = [1, 2, 3, 4, 5]
+# print(linear_search(my_list, 3))
+
+
+
+# def binary_search(l, x):
+#     # le = [1, 2, 3, 4, 5]
+#     left = 0
+#     right = len(l)-1
+#     while left <= right:
+#         mid = (left + right)//2
+#         if l[mid] == x:
+#             return mid
+#         if l[mid] < x:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return -1
+#
+# le = [1, 2, 3, 4, 5]
+# print(binary_search(le, 5))
+
+
+
+# def binary_search(l, x):
+#     left = 0
+#     right = len(l) - 1
+#     while left <= right:
+#         mid = (left + right)//2
+#         if l[mid] == x:
+#             return mid
+#         if l[mid] < x:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return -1
+#
+# li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(binary_search(li, 2))
+
+
+# foor loop
+
+def binary_search(l, x):
+    left = 0
+    right = len(l)
+    for i in range(left, right):
+        mid = (left+right)//2
+        if l[mid] == x:
+            return mid
+        if l[mid] < x:
+            left = mid+1
+        else:
+            right = mid-1
+    return -1
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],9))
+
+
